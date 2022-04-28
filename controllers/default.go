cdecd0cc7a5be71360f1a85d4bb3d60bc3c8d999
daichaochao <1078638034@qq.com>
2022-04-28 10:49:02 +0800

    push test

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -9,6 +10,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	fmt.Printf("1111")
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
